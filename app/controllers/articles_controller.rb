b8807becb1df2c57d68eb7cf717ab0da95d80683
Marcelo  Cavassani <marceloscavassani@gmail.com>
2017-01-18 10:32:22 -0200

    criacao de edit e show para articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,35 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Artigo criado com sucesso"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Artigo foi atualizado com sucesso"
+    else
+      render 'edit'
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
 
 end
