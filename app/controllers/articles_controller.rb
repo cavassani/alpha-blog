08e3bad8fbfa3c1816db5eb8cd47c339d03be6ed
Marcelo Cavassani <marceloscavassani@gmail.com>
2017-01-20 14:37:28 -0200

    ajsutes em controller de articles para nao ficar repetitivo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -6,8 +7,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -21,7 +23,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Artigo foi atualizado com sucesso"
     else
@@ -30,17 +32,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "artigo deletado com sucesso"
     redirect_to articles_path
   end
-  
+
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
