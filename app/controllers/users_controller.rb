a05c8ed0799a7d2cadb05ca5c055caf71313ddc0
Marcelo  Cavassani <marceloscavassani@gmail.com>
2017-02-02 23:48:35 -0200

    nao sei porque :D

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   before_action :require_admin, only: [:destroy]
-  
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def new
     @user = User.new
   end
@@ -33,11 +33,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -49,23 +49,23 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
   def set_user
     @user = User.find(params[:id])
   end
-  
+
   def require_same_user
     if logged_in? && current_user != @user and !current_user.admin
       flash[:danger] = "Você só pode editar sua propria conta."
       redirect_to root_path
     end
   end
-  
+
   def require_admin
     if logged_in? and !current_user.admin?
       flash[:danger] = "Apenas Administradores podem realizar esta ação.
       redirect_to root_path"
     end
   end
-    
+
 end
