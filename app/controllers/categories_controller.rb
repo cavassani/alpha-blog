0bed93c56ba11080a306547188cf1cb4c2314f41
Marcelo  Cavassani <marceloscavassani@gmail.com>
2017-02-24 22:44:49 -0300

    atualizando as views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,11 +3,11 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def new
     @category = Category.new
   end
-  
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -17,12 +17,13 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
-  
-  
+
+
   def show
-    
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
-  
+
   private
   def category_params
     params.require(:category).permit(:name)
@@ -34,5 +35,5 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     end
   end
-    
+
  end
